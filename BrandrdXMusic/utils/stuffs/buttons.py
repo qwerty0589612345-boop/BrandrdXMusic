cefb71f48cda2d39f4e8c21e02594dd13b9595db
Mohamed05896 <125174475+Mohamed05896@users.noreply.github.com>
2025-12-23 23:03:21 +0200

    Update buttons.py

--- BrandrdXMusic/utils/stuffs/buttons.py
+++ BrandrdXMusic/utils/stuffs/buttons.py
@@ -2,18 +2,40 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram import Client, filters, enums 
 
 class BUTTONS(object):
-    MBUTTON = [[InlineKeyboardButton("CʜᴀᴛGPT", callback_data="mplus HELP_ChatGPT"),InlineKeyboardButton("Hɪsᴛᴏʀʏ", callback_data="mplus HELP_History"),InlineKeyboardButton("Rᴇᴇʟ", callback_data="mplus HELP_Reel")],
-    [InlineKeyboardButton("Tᴀɢ-Aʟʟ", callback_data="mplus HELP_TagAll"),
-    InlineKeyboardButton("Iɴꜰᴏ", callback_data="mplus HELP_Info"),InlineKeyboardButton("Exᴛʀᴀ", callback_data="mplus HELP_Extra")],
-    [InlineKeyboardButton("ᴄᴏᴜᴘʟᴇꜱ", callback_data="mplus HELP_Couples"),
-    InlineKeyboardButton("Aᴄᴛɪᴏɴ", callback_data="mplus HELP_Action"),InlineKeyboardButton("Sᴇᴀʀᴄʜ", callback_data="mplus HELP_Search")],    
-    [InlineKeyboardButton("ғᴏɴᴛ", callback_data="mplus HELP_Font"),
-    InlineKeyboardButton("Bᴏᴛs", callback_data="mplus HELP_Bots"),InlineKeyboardButton("Ⓣ-ɢʀᴀᴘʜ", callback_data="mplus HELP_TG")],
-    [InlineKeyboardButton("Sᴏᴜʀᴄᴇ", callback_data="mplus HELP_Source"),
-    InlineKeyboardButton("Tʀᴜᴛʜ-ᗪᴀʀᴇ", callback_data="mplus HELP_TD"),InlineKeyboardButton("Qᴜɪᴢ", callback_data="mplus HELP_Quiz")], 
-    [InlineKeyboardButton("ᴛᴛs", callback_data="mplus HELP_TTS"),
-    InlineKeyboardButton("Rᴀᴅɪᴏ", callback_data="mplus HELP_Radio"),InlineKeyboardButton("ǫᴜᴏᴛʟʏ", callback_data="mplus HELP_Q")],          
-    [InlineKeyboardButton("◁", callback_data=f"settings_back_helper"),
-     InlineKeyboardButton("↻ ʙᴀᴄᴋ ↻", callback_data=f"mbot_cb"), 
-    InlineKeyboardButton("▷", callback_data=f"managebot123 settings_back_helper"),
-    ]]
+    MBUTTON = [
+        [
+            InlineKeyboardButton("شات GPT", callback_data="mplus HELP_ChatGPT"),
+            InlineKeyboardButton("السجل", callback_data="mplus HELP_History"),
+            InlineKeyboardButton("ريلز", callback_data="mplus HELP_Reel")
+        ],
+        [
+            InlineKeyboardButton("منشن الكل", callback_data="mplus HELP_TagAll"),
+            InlineKeyboardButton("معلومات", callback_data="mplus HELP_Info"),
+            InlineKeyboardButton("إضافات", callback_data="mplus HELP_Extra")
+        ],
+        [
+            InlineKeyboardButton("كابلز", callback_data="mplus HELP_Couples"),
+            InlineKeyboardButton("أكشن", callback_data="mplus HELP_Action"),
+            InlineKeyboardButton("بحث", callback_data="mplus HELP_Search")
+        ],
+        [
+            InlineKeyboardButton("الخطوط", callback_data="mplus HELP_Font"),
+            InlineKeyboardButton("بوتات", callback_data="mplus HELP_Bots"),
+            InlineKeyboardButton("تيليجراف", callback_data="mplus HELP_TG")
+        ],
+        [
+            InlineKeyboardButton("المصدر", callback_data="mplus HELP_Source"),
+            InlineKeyboardButton("صراحة / جرأة", callback_data="mplus HELP_TD"),
+            InlineKeyboardButton("اختبار", callback_data="mplus HELP_Quiz")
+        ],
+        [
+            InlineKeyboardButton("تحويل صوت", callback_data="mplus HELP_TTS"),
+            InlineKeyboardButton("راديو", callback_data="mplus HELP_Radio"),
+            InlineKeyboardButton("اقتباسات", callback_data="mplus HELP_Q")
+        ],
+        [
+            InlineKeyboardButton("◁", callback_data="settings_back_helper"),
+            InlineKeyboardButton("↻ رجوع ↻", callback_data="mbot_cb"),
+            InlineKeyboardButton("▷", callback_data="managebot123 settings_back_helper")
+        ]
+    ]
