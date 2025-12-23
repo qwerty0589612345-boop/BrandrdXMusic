33fe8a527dc04a545f4906339da5fbc64b023037
Mohamed05896 <125174475+Mohamed05896@users.noreply.github.com>
2025-12-23 21:46:07 +0200

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,6 +1,3 @@
-import os
-from typing import List
-
 import yaml
 
 LOGGERS = "Branded_king__robot"
@@ -9,28 +6,11 @@ languages = {}
 languages_present = {}
 
 
-def get_string(lang: str):
-    return languages[lang]
+def get_string(lang: str = "ar"):
+    return languages["ar"]
 
 
-for filename in os.listdir(r"./strings/langs/"):
-    if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
-        )
-        languages_present["en"] = languages["en"]["name"]
-    if filename.endswith(".yml"):
-        language_name = filename[:-4]
-        if language_name == "en":
-            continue
-        languages[language_name] = yaml.safe_load(
-            open(r"./strings/langs/" + filename, encoding="utf8")
-        )
-        for item in languages["en"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
-    try:
-        languages_present[language_name] = languages[language_name]["name"]
-    except:
-        print("There is some issue with the language file inside bot.")
-        exit()
+# تحميل اللغة العربية فقط
+with open("./strings/langs/ar.yml", encoding="utf8") as f:
+    languages["ar"] = yaml.safe_load(f)
+    languages_present["ar"] = languages["ar"].get("name", "العربية")
