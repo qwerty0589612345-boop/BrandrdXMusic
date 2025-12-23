3642ee781d16ca78a9102abbbeca35c6338469e4
Mohamed05896 <125174475+Mohamed05896@users.noreply.github.com>
2025-12-23 21:50:37 +0200

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -9,28 +9,31 @@ languages = {}
 languages_present = {}
 
 
-def get_string(lang: str):
+def get_string(lang: str = "ar"):
     return languages[lang]
 
 
 for filename in os.listdir(r"./strings/langs/"):
+    # تحميل اللغة العربية مرة واحدة
     if "ar" not in languages:
         languages["ar"] = yaml.safe_load(
             open(r"./strings/langs/ar.yml", encoding="utf8")
         )
         languages_present["ar"] = languages["ar"]["name"]
+
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "ar":
+
+        # تجاهل أي ملف لغة غير العربية
+        if language_name != "ar":
             continue
+
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        for item in languages["en"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["ar"][item]
+
     try:
         languages_present[language_name] = languages[language_name]["name"]
     except:
-        print("There is some issue with the language file inside bot.")
+        print("في مشكلة داخل ملف اللغة، تأكد إن ar.yml سليم.")
         exit()
