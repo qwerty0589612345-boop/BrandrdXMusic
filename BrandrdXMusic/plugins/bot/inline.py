49cd7d3f425a2ed518c20a7d2c9cc217d695485e
Mohamed05896 <125174475+Mohamed05896@users.noreply.github.com>
2025-12-24 01:10:54 +0200

    Update inline.py

--- BrandrdXMusic/plugins/bot/inline.py
+++ BrandrdXMusic/plugins/bot/inline.py
@@ -14,14 +14,20 @@ from config import BANNED_USERS
 async def inline_query_handler(client, query):
     text = query.query.strip().lower()
     answers = []
+
     if text.strip() == "":
         try:
-            await client.answer_inline_query(query.id, results=answer, cache_time=10)
+            await client.answer_inline_query(
+                query.id,
+                results=answer,
+                cache_time=10
+            )
         except:
             return
     else:
         a = VideosSearch(text, limit=20)
         result = (await a.next()).get("result")
+
         for x in range(15):
             title = (result[x]["title"]).title()
             duration = result[x]["duration"]
@@ -31,27 +37,32 @@ async def inline_query_handler(client, query):
             channel = result[x]["channel"]["name"]
             link = result[x]["link"]
             published = result[x]["publishedTime"]
-            description = f"{views} | {duration} ᴍɪɴᴜᴛᴇs | {channel}  | {published}"
+
+            description = f"{views} | {duration} دقيقة | {channel} | {published}"
+
             buttons = InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton(
-                            text="ʏᴏᴜᴛᴜʙᴇ 🎄",
+                            text="يوتيوب 🎧",
                             url=link,
                         )
                     ],
                 ]
             )
+
             searched_text = f"""
-❄ <b>ᴛɪᴛʟᴇ :</b> <a href={link}>{title}</a>
+❄ <b>العنوان :</b> <a href={link}>{title}</a>
 
-⏳ <b>ᴅᴜʀᴀᴛɪᴏɴ :</b> {duration} ᴍɪɴᴜᴛᴇs
-👀 <b>ᴠɪᴇᴡs :</b> <code>{views}</code>
-🎥 <b>ᴄʜᴀɴɴᴇʟ :</b> <a href={channellink}>{channel}</a>
-⏰ <b>ᴘᴜʙʟɪsʜᴇᴅ ᴏɴ :</b> {published}
+⏳ <b>المدة :</b> {duration} دقيقة
+👀 <b>المشاهدات :</b> <code>{views}</code>
+🎥 <b>القناة :</b> <a href={channellink}>{channel}</a>
+⏰ <b>تاريخ النشر :</b> {published}
 
+<u><b>➻ وضع البحث السريع بواسطة {app.name}</b></u>
+<b>➻ sᴏᴜʀᴄᴇ : بُودَا | ʙᴏᴅᴀ</b>
+"""
 
-<u><b>➻ ɪɴʟɪɴᴇ sᴇᴀʀᴄʜ ᴍᴏᴅᴇ ʙʏ {app.name}</b></u>"""
             answers.append(
                 InlineQueryResultPhoto(
                     photo_url=thumbnail,
@@ -62,7 +73,11 @@ async def inline_query_handler(client, query):
                     reply_markup=buttons,
                 )
             )
+
         try:
-            return await client.answer_inline_query(query.id, results=answers)
+            return await client.answer_inline_query(
+                query.id,
+                results=answers
+            )
         except:
             return
