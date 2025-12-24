0c081f66121dd4d4bb569ef50a02c5728c4c0e51
Mohamed05896 <125174475+Mohamed05896@users.noreply.github.com>
2025-12-24 20:36:38 +0200

    Update paste.py

--- BrandrdXMusic/plugins/tools/paste.py
+++ BrandrdXMusic/plugins/tools/paste.py
@@ -12,50 +12,45 @@ from BrandrdXMusic import app
 from BrandrdXMusic.utils.errors import capture_err
 from BrandrdXMusic.utils.pastebin import HottyBin
 
+# ➻ sᴏᴜʀᴄᴇ : بُودَا | ʙᴏᴅَا
+
 pattern = re.compile(r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$")
 
-
-async def isPreviewUp(preview: str) -> bool:
-    for _ in range(7):
-        try:
-            async with session.head(preview, timeout=2) as resp:
-                status = resp.status
-                size = resp.content_length
-        except asyncio.exceptions.TimeoutError:
-            return False
-        if status == 404 or (status == 200 and size == 0):
-            await asyncio.sleep(0.4)
-        else:
-            return True if status == 200 else False
-    return False
-
-
-@app.on_message(filters.command("paste"))
+@app.on_message(filters.command(["paste", "رفع", "باست"]))
 @capture_err
 async def paste_func(_, message):
     if not message.reply_to_message:
-        return await message.reply_text("Reply To A Message With /paste")
-    m = await message.reply_text("Pasting...")
+        return await message.reply_text("**يـا حـبـيـب قـلـبـي رد عـلـى الـنـص أو الـمـلـف بـأمـر الـرفـع.. 🔗**")
+    
+    m = await message.reply_text("**جـاري الـرفـع الـآن.. صـبـرك يـا رايـق.. ⏳**")
+    
     if message.reply_to_message.text:
         content = str(message.reply_to_message.text)
     elif message.reply_to_message.document:
         document = message.reply_to_message.document
         if document.file_size > 1048576:
-            return await m.edit("You can only paste files smaller than 1MB.")
+            return await m.edit("**يـا بـطـل الـمـلـف كـبـيـر أوي.. لازم يـكـون أقـل مـن 1 مـيـجـا.. ❌**")
         if not pattern.search(document.mime_type):
-            return await m.edit("Only text files can be pasted.")
+            return await m.edit("**مـقـدرش أرفـع غـيـر الـمـلـفـات الـنـصـيـة بـس.. 📑**")
+        
         doc = await message.reply_to_message.download()
         async with aiofiles.open(doc, mode="r") as f:
             content = await f.read()
         os.remove(doc)
+    
     link = await HottyBin(content)
-    preview = link 
     button = InlineKeyboard(row_width=1)
-    button.add(InlineKeyboardButton(text="• ᴘᴀsᴛᴇ ʟɪɴᴋ •", url=link))
+    button.add(InlineKeyboardButton(text="✧ رابـط الـنـص الـمـرفـوع ✧", url=link))
 
     await m.delete()
     try:
-        await message.reply("ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴘᴀsᴛᴇ ʟɪɴᴋ :", quote=False, reply_markup=button)
+        await message.reply(
+            f"**تـم الـرفـع بـنـجـاح يـا رايـق.. ✨**\n\n**➻ sᴏᴜʀᴄᴇ : بُودَا | ʙᴏᴅَا**", 
+            quote=False, 
+            reply_markup=button
+        )
         
     except Exception:
         pass
+
+# ➻ sᴏᴜʀᴄᴇ : بُودَا | ʙᴏᴅَا
