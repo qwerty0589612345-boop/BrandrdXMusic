ecf2c7b2670aed0fbebb541d9bee4502d6b66273
Mohamed05896 <125174475+Mohamed05896@users.noreply.github.com>
2025-12-24 20:48:45 +0200

    Update autoend.py

--- BrandrdXMusic/plugins/sudo/autoend.py
+++ BrandrdXMusic/plugins/sudo/autoend.py
@@ -5,20 +5,25 @@ from BrandrdXMusic import app
 from BrandrdXMusic.misc import SUDOERS
 from BrandrdXMusic.utils.database import autoend_off, autoend_on
 
+# ➻ sᴏᴜʀᴄᴇ : بُودَا | ʙᴏᴅَا
 
-@app.on_message(filters.command("autoend") & SUDOERS)
+@app.on_message(filters.command(["autoend", "الإنهاء_التلقائي"]) & SUDOERS)
 async def auto_end_stream(_, message: Message):
-    usage = "<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
+    usage = "**الـاسـتـخـدام :**\n\n/autoend [تـفـعـيـل | تـعـطـيـل]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
+    
     state = message.text.split(None, 1)[1].strip().lower()
-    if state == "enable":
+    
+    if state in ["enable", "تـفـعـيـل"]:
         await autoend_on()
         await message.reply_text(
-            "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
+            "**تـم تـفـعـيـل الـإنـهـاء الـتـلـقـائـي بـنـجـاح.. ✨**\n\n**سـيـقـوم الـمـسـاعـد بـمـغـادره الـمـكـالـمـة تـلـقـائـيـاً عـنـد خـروج الـأعـضـاء.**"
         )
-    elif state == "disable":
+    elif state in ["disable", "تـعـطـيـل"]:
         await autoend_off()
-        await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
+        await message.reply_text("**تـم تـع_طـيـل الـإنـهـاء الـتـلـقـائـي بـنـجـاح.. ✨**")
     else:
         await message.reply_text(usage)
+
+# ➻ sᴏᴜʀᴄᴇ : بُودَا | ʙᴏᴅَا
